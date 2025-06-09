8b7cbcfb75acf2a27ca03c2e6ffa1543b45e8459
villarzz <hugovilar04@gmail.com>
2025-06-09 20:39:12 -0300

    FEAT:(pacientes) - adicionando requisição para obter pacientes

--- src/app/pages/pacientes/pacientes.component.ts
+++ src/app/pages/pacientes/pacientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PacientesService } from './services/pacientes.service';
 
 @Component({
   selector: 'app-pacientes',
@@ -6,19 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pacientes.component.css'],
 })
 export class PacientesComponent implements OnInit {
+  cpf!: number;
   nome!: string;
   convenio!: string;
-  cpf!: string;
   atendimento!: string;
   pacientes: any[] = [];
   dataNascimento!: string;
+
+  constructor(private readonly _pacientesService: PacientesService) {}
 
   ngOnInit(): void {
     this.getPacientes();
   }
 
   getPacientes(){
-
+    this._pacientesService
+      .getPacientes(this.nome, this.convenio, this.cpf, this.dataNascimento)
+      .subscribe((data) => {
+        this.pacientes = data;
+      });
   }
 
   postPaciente(){
---
+++ src/app/pages/pacientes/services/pacientes.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PacientesService {
+  constructor(private http: HttpClient) {}
+
+  public getPacientes(
+  nomePaciente?: string,
+  convenio?: string,
+  cpf?: number,
+  dataNascimento?: string
+) {
+  let params = new HttpParams();
+
+  if (nomePaciente) {
+    params = params.set('nomePaciente', nomePaciente);
+  }
+
+  if (convenio) {
+    params = params.set('convenio', convenio);
+  }
+
+  if (cpf) {
+    params = params.set('cpf', cpf.toString());
+  }
+
+  if (dataNascimento) {
+    params = params.set('dataNascimento', dataNascimento);
+  }
+
+  const token = localStorage.getItem('token');
+
+  const headers = new HttpHeaders({
+    Authorization: `Bearer ${token}`
+  });
+
+  return this.http.get<any[]>(
+    'https://localhost:7174/api/Paciente/obter-pacientes',
+    { params, headers }
+  );
+}
+
+}
